f17cbd1906779375380601aee33995685127907f
ilya <ilya_ovchinnikovv@mail.ru>
2024-11-25 20:10:14 +0500

    create client and common

--- Server/obj/Debug/netcoreapp3.1/Server.AssemblyInfo.cs
+++ Server/obj/Debug/netcoreapp3.1/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99662516dede5f7589a6c8ed6893c6cbe369fad5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
