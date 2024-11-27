be8d2a5e0daf819535f163a5f65aac5439d8fb3a
alenafilimonova98@gmail.com <natalyamozhaeva1107@gmail.com>
2024-11-27 19:22:26 +0500

    add auth page

--- Client/obj/Debug/netcoreapp3.1/Client.AssemblyInfo.cs
+++ Client/obj/Debug/netcoreapp3.1/Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d66d661e5588646257d14e09d2445b9025f9fb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8ebfc181b5d1b7675f11d6c095d092cf2a26d76")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ FtpWPF/auth.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FtpWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для auth.xaml
+    /// </summary>
+    public partial class auth : Page
+    {
+        public auth()
+        {
+            InitializeComponent();
+            EtherealWind.Tools.ImageExtensions imageExtensions = new EtherealWind.Tools.ImageExtensions();
+            string gifPath = System.IO.Path.Combine(Directory.GetCurrentDirectory() + "/Gif/");
+            BitmapImage[] frames = imageExtensions.BitmapsInit(gifPath, "png", 20);
+            imageExtensions.PlayAsGif(bottomGif, frames, 5.0, true);
+        }
+
+        private void Login(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
